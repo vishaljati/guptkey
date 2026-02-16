4f989e4f6d15c1c0a4b63184827fac8ef62499c3
vishaljati <jativishal120@gmail.com>
2026-02-16 13:19:13 +0530

    feat: Add redux implementation in password vault state management

---
+++ frontend/src/features/vaultSlicer.ts
@@ -0,0 +1,72 @@
+import { type VaultEntry, Vault } from '../types/vault.types';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface VaultState {
+    vault: Vault | null;
+    isUnlocked: boolean;
+}
+
+const initialState: VaultState = {
+    vault: null,
+    isUnlocked: false,
+};
+
+const vaultSlice = createSlice({
+    name: "vault",
+    initialState,
+    reducers: {
+
+        //Unlock vault after successful decrypt
+        setVault(state, action: PayloadAction<Vault>) {
+            state.vault = action.payload;
+            state.isUnlocked = true;
+        },
+
+        //Lock vault (logout or manual lock)
+        clearVault(state) {
+            state.vault = null;
+            state.isUnlocked = false;
+        },
+
+        //Add entry
+        addEntry(state, action: PayloadAction<VaultEntry>) {
+            if (!state.vault) return;
+
+            state.vault.entries.unshift(action.payload);
+            state.vault.updatedAt = Date.now();
+        },
+
+        //Update entry
+        updateEntry(state, action: PayloadAction<VaultEntry>) {
+            if (!state.vault) return;
+
+            const index = state.vault.entries.findIndex(
+                (e) => e.id === action.payload.id
+            );
+
+            if (index !== -1) {
+                state.vault.entries[index] = action.payload;
+                state.vault.updatedAt = Date.now();
+            }
+        },
+
+        // Delete entry
+        deleteEntry(state, action: PayloadAction<string>) {
+            if (!state.vault) return;
+
+            state.vault.entries = state.vault.entries.filter(
+                (e) => e.id !== action.payload
+            );
+
+            state.vault.updatedAt = Date.now();
+        },
+    },
+});
+
+export const { setVault,
+    clearVault,
+    addEntry,
+    updateEntry,
+    deleteEntry } = vaultSlice.actions;
+
+export default vaultSlice.reducer;
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "@/features/authSlicer"
+import vaultReducer from "@/features/vaultSlicer"
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    vault: vaultReducer,
   }
 })
 
