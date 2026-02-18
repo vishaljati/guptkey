0cdf5febf95f8420126141099b0366ca53218d88
vishaljati <jativishal120@gmail.com>
2026-02-18 19:32:22 +0530

    fix: remove catch part of all controller to send the error message into frontend

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -33,158 +33,142 @@ const generateAccessAndRefreshTokens = async (userId: Types.ObjectId) => {
 }
 
 const registerUser = AsyncHandler(async (req: Request, res: Response) => {
-    try {
-        const { name, email, password, encryptedData, iv, salt } = req.body
-        if (!name || !email) {
-            throw new ApiError(400, "Name and Email is required")
-        }
-        if (!password) {
-            throw new ApiError(400, "Password is required")
-        }
-        if (!encryptedData || !iv || !salt) {
-            throw new ApiError(400, "Encrypted data, IV and salt are required")
-        }
 
-        const existedUser = await User.findOne({ email })
-
-        if (existedUser) {
-            const existedEncryptedPassword = await EncryptedPassword.findOne({ userId: existedUser._id })
-            if (existedEncryptedPassword) {
-                throw new ApiError(409, "Username or Email already exists with encrypted password vault")
-            }
-        }
-        if (existedUser) {
-            throw new ApiError(409, "Email already exists")
-        }
-
-        const newUser = await User.create({
-            name,
-            email,
-            password,
-        })
-        const createdUser = await User.findOne({ email }).select("-password -refreshToken -__v")
-        if (!createdUser) {
-            throw new ApiError(500, "User creation failed in DB")
-        }
-        const createdEncryptedPasswordVault = await EncryptedPassword.create({
-            userId: createdUser._id,
-            encryptedData,
-            iv,
-            salt
-        })
-
-        if (!createdEncryptedPasswordVault) {
-            throw new ApiError(500, "Encrypted password vault creation failed in DB")
-        }
-
-        //NOTE:Sending registration mail
-        return res.status(201).json(
-            new ApiResponse
-                (201, "User registered successfully", createdUser))
-
-    } catch (error) {
-        console.error("Something went wrong while registering user", error)
-        return
+    const { name, email, password, encryptedData, iv, salt } = req.body
+    if (!name || !email) {
+        throw new ApiError(400, "Name and Email is required")
+    }
+    if (!password) {
+        throw new ApiError(400, "Password is required")
+    }
+    if (!encryptedData || !iv || !salt) {
+        throw new ApiError(400, "Encrypted data, IV and salt are required")
     }
 
+    const existedUser = await User.findOne({ email })
+
+    if (existedUser) {
+        const existedEncryptedPassword = await EncryptedPassword.findOne({ userId: existedUser._id })
+        if (existedEncryptedPassword) {
+            throw new ApiError(409, "Email already exists with encrypted password vault")
+        }
+    }
+    if (existedUser) {
+        throw new ApiError(409, "Email already exists")
+    }
+    await User.create({
+        name,
+        email,
+        password,
+    })
+    const createdUser = await User.findOne({ email }).select("-password -refreshToken -__v")
+    if (!createdUser) {
+        throw new ApiError(500, "User creation failed in DB")
+    }
+    const createdEncryptedPasswordVault = await EncryptedPassword.create({
+        userId: createdUser._id,
+        encryptedData,
+        iv,
+        salt
+    })
+
+    if (!createdEncryptedPasswordVault) {
+        throw new ApiError(500, "Encrypted password vault creation failed in DB")
+    }
+
+    //NOTE:Sending registration mail
+    return res.status(201).json(
+        new ApiResponse
+            (201, "User registered successfully", createdUser))
 
 })
 
 const loginUser = AsyncHandler(async (req: Request, res: Response) => {
-    try {
-        const { email, password } = req.body || {}
-        if (!email || !password) {
-            throw new ApiError(400, "Email and Password is required")
 
-        }
+    const { email, password } = req.body || {}
+    if (!email || !password) {
+        throw new ApiError(400, "Email and Password is required")
 
-        const user = await User.findOne({ email }).select("+password");
-
-        if (!user) {
-            throw new ApiError(404, "User not found")
-        }
-
-        const isPasswordValid = await user.isPasswordCorrect(password)
-
-        if (!isPasswordValid) {
-            throw new ApiError(401, "Invalid password")
-        }
-        const userId = user._id
-
-        const userEncryptedPasswordVault = await EncryptedPassword.findOne({ userId: userId }).select("-__v -createdAt -updatedAt -userId")
-
-        if (!userEncryptedPasswordVault) {
-            throw new ApiError(404, "Encrypted password vault not found for the user")
-        }
-        const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(userId);
-
-        if (!accessToken || !refreshToken) {
-            throw new ApiError(500, "ACCESS & REFRESH TOKEN GENERATION FAILED")
-        }
-
-        const loggedInUser = await User.findById(userId).select("-password -refreshToken")
-
-        const options = {
-            httpOnly: true,
-            secure: true
-        }
-
-        const responseData = {
-            userData: {
-                _id: loggedInUser?._id,
-                name: loggedInUser?.name,
-                email: loggedInUser?.email
-            },
-            encryptedData: userEncryptedPasswordVault.encryptedData,
-            iv: userEncryptedPasswordVault.iv,
-            salt: userEncryptedPasswordVault.salt
-        }
-
-        return res.status(200)
-            .cookie("accessToken", accessToken, options)
-            .cookie("refreshToken", refreshToken, options)
-            .json(new ApiResponse(200, "User logged in successfully", responseData))
-
-
-    } catch (error) {
-        console.log("ERROR :", error);
-        throw new ApiError(500, "Something went wrong while login user")
     }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password")
+    }
+    const userId = user._id
+
+    const userEncryptedPasswordVault = await EncryptedPassword.findOne({ userId: userId }).select("-__v -createdAt -updatedAt -userId")
+
+    if (!userEncryptedPasswordVault) {
+        throw new ApiError(404, "Encrypted password vault not found for the user")
+    }
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(userId);
+
+    if (!accessToken || !refreshToken) {
+        throw new ApiError(500, "ACCESS & REFRESH TOKEN GENERATION FAILED")
+    }
+
+    const loggedInUser = await User.findById(userId).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    const responseData = {
+        userData: {
+            _id: loggedInUser?._id,
+            name: loggedInUser?.name,
+            email: loggedInUser?.email
+        },
+        encryptedData: userEncryptedPasswordVault.encryptedData,
+        iv: userEncryptedPasswordVault.iv,
+        salt: userEncryptedPasswordVault.salt
+    }
+
+    return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(200, "User logged in successfully", responseData))
 
 })
 
 const logoutUser = AsyncHandler(async (req: Request, res: Response) => {
     const userId = req.user._id
-    try {
-        await User.findByIdAndUpdate(
-            userId,
-            {
-                $set: {
-                    refreshToken: undefined
-                }
-            },
-            {
-                new: true
-            }
 
-        )
-        const options = {
-            httpOnly: true,
-            secure: true
+    await User.findByIdAndUpdate(
+        userId,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
         }
 
-        return res.status(200)
-            .clearCookie("accessToken", options)
-            .clearCookie("refreshToken", options)
-            .json(new ApiResponse(
-                200,
-                "User Logged out"
-
-            ))
-    } catch (error) {
-        console.error("Error while log out", error)
-        return
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
     }
+
+    return res.status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(
+            200,
+            "User Logged out"
+
+        ))
+
 })
 
 const refreshAccessToken = AsyncHandler(async (req: Request, res: Response) => {
@@ -200,46 +184,44 @@ const refreshAccessToken = AsyncHandler(async (req: Request, res: Response) => {
         throw new ApiError(401, "Unauthorized request: Refresh token missing or invalid");
     }
 
-    try {
-        const decodedToken = jwt.verify(
-            incomingRefreshToken,
-            process.env.REFRESH_TOKEN_SECRET as string
-        ) as RefreshTokenPayload
 
-        const user = await User.findById(decodedToken._id)
+    const decodedToken = jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET as string
+    ) as RefreshTokenPayload
 
-        if (!user) {
-            throw new ApiError(401, "Invalid refresh token");
-        }
+    const user = await User.findById(decodedToken._id)
 
-        if (incomingRefreshToken !== user?.refreshToken) {
-            throw new ApiError(401, "Refresh token is expired or used");
-        }
-
-        const option = {
-            httpOnly: true,
-            secure: true
-        }
-
-        const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
-
-
-        return res.status(200)
-            .cookie("accessToken", accessToken, option)
-            .cookie("refreshToken", refreshToken, option)
-            .json(
-                new ApiResponse(
-                    200,
-                    "Access Token Refreshed Successfully",
-                    {
-                        accessToken: accessToken,
-                        refreshToken: refreshToken
-                    },
-                )
-            )
-    } catch (error) {
-        return res.status(403).json({ message: "Invalid or expired refresh token" });
+    if (!user) {
+        throw new ApiError(401, "Invalid refresh token");
     }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+        throw new ApiError(401, "Refresh token is expired or used");
+    }
+
+    const option = {
+        httpOnly: true,
+        secure: true
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+
+    return res.status(200)
+        .cookie("accessToken", accessToken, option)
+        .cookie("refreshToken", refreshToken, option)
+        .json(
+            new ApiResponse(
+                200,
+                "Access Token Refreshed Successfully",
+                {
+                    accessToken: accessToken,
+                    refreshToken: refreshToken
+                },
+            )
+        )
+
 })
 
 export {
--- backend/src/controllers/passwordVault.controllers.ts
+++ backend/src/controllers/passwordVault.controllers.ts
@@ -11,70 +11,58 @@ const updatePasswordVault = AsyncHandler(async (req: Request, res: Response) => 
 
     const { encryptedData, iv, salt } = req.body;
 
-    try {
 
-        if (!encryptedData || !iv || !salt) {
-            throw new ApiError(400, "Encyrpted data is required")
-        }
 
-        const updatedPasswordVault =
-            await EncryptedPassword.findOneAndUpdate(
-                {
-                    userId: new mongoose.Types.ObjectId(userId)
-                },
-                {
-                    $set: {
-                        encryptedData,
-                        iv,
-                        salt
-                    }
-                },
-                {
-                    new: true,
-                    runValidators: true
-                }
-
-            )
-        if (!updatedPasswordVault) {
-            throw new ApiError(500, "Password vault updation failed")
-        }
-
-        return res.status(200).json(new ApiResponse(200, "Password vault updated successfully", updatedPasswordVault))
-    } catch (error) {
-        console.log("Error :", error);
-        throw new ApiError(500, "Something went wrong while updation vault")
+    if (!encryptedData || !iv || !salt) {
+        throw new ApiError(400, "Encyrpted data is required")
     }
+
+    const updatedPasswordVault =
+        await EncryptedPassword.findOneAndUpdate(
+            {
+                userId: new mongoose.Types.ObjectId(userId)
+            },
+            {
+                $set: {
+                    encryptedData,
+                    iv,
+                    salt
+                }
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+
+        )
+    if (!updatedPasswordVault) {
+        throw new ApiError(500, "Password vault updation failed")
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Password vault updated successfully", updatedPasswordVault))
 
 })
 
 const getPasswordVault = AsyncHandler(async (req: Request, res: Response) => {
     const userId = req.user._id;
-    try {
-        const passwordVault = await EncryptedPassword.findOne({ userId: new mongoose.Types.ObjectId(userId) });
-        if (!passwordVault) {
-            throw new ApiError(404, "Password vault not found")
-        }
-        return res.status(200).json(new ApiResponse(200, "Password fetched successfully", passwordVault))
-    } catch (error) {
-        console.log("Error :", error);
-        throw new ApiError(500, "Something went wrong while fetching password vault")
 
+    const passwordVault = await EncryptedPassword.findOne({ userId: new mongoose.Types.ObjectId(userId) });
+    if (!passwordVault) {
+        throw new ApiError(404, "Password vault not found")
     }
+    return res.status(200).json(new ApiResponse(200, "Password fetched successfully", passwordVault))
 });
 
 const deletePasswordVault = AsyncHandler(async (req: Request, res: Response) => {
     const userId = req.user._id
-    try {
-        //otp logic
-        const deletedPasswordVault = await EncryptedPassword.deleteOne({ userId: new mongoose.Types.ObjectId(userId) })
-        if (!deletedPasswordVault) {
-            throw new ApiError(500, "Server error: Vault deletion failed")
-        }
-        return res.status(200).json(new ApiResponse(200, "Password vault deleted successfully"))
-    } catch (error) {
-        console.log("Error :", error);
-        throw new ApiError(500, "Something went wrong while deleting password vault")
+
+    //otp logic
+    const deletedPasswordVault = await EncryptedPassword.deleteOne({ userId: new mongoose.Types.ObjectId(userId) })
+    if (!deletedPasswordVault) {
+        throw new ApiError(500, "Server error: Vault deletion failed")
     }
+    return res.status(200).json(new ApiResponse(200, "Password vault deleted successfully"))
+
 })
 
 export {
