bae365784c9c682ac34a1649d532de1c3548dfde
vishaljati <jativishal120@gmail.com>
2026-02-25 01:41:41 +0530

    feat: Remove welcome email

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 import { Types } from "mongoose";
 import { type RefreshTokenPayload } from "../types/jwt.types.js";
 import { EncryptedPassword } from "../models/vault.model.js";
-import { sendWelcomeEmail } from "../utils/sendEmail.js";
+
 
 const generateAccessAndRefreshTokens = async (userId: Types.ObjectId) => {
   try {
@@ -74,12 +74,6 @@ const registerUser = AsyncHandler(async (req: Request, res: Response) => {
 
   if (!createdEncryptedPasswordVault) {
     throw new ApiError(500, "Encrypted password vault creation failed in DB");
-  }
-
-  try {
-    await sendWelcomeEmail(createdUser.email, createdUser.name);
-  } catch (error) {
-    console.error("Welcome Email failed:", error);
   }
 
   return res
